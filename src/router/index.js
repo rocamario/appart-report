93f33cbedf5921a7f194f1e322bed3196039e68a
Mario Roca <marioroca@MacBook-Pro-di-Mario.local>
2024-06-30 16:05:03 +0200

    Add login and registration pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Login from '../views/Login.vue'
+import Registration from '../views/Registration.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: Registration,
   },
   // Add other routes as needed
 ];
